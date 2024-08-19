4b87e1debef5701eda97e734d42636f7d4b86510
Garcia <Ismael.Garcia@aptiv.com>
2024-08-19 11:09:22 -0600

    competed finder bash script

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-if [$# -lt 2]
+FILEDIR=$1
+SRCHSTR=$2 
+
+if [ $# -lt 2 ]
 then
-    echo "Missing Argument"
+    echo "Missing Argument, please  try again with valid inputs"
+    exit 1
+    
 else
+    echo "Looking for file ${SRCHSTR}"
+    cd "$FILEDIR" || ( echo "${FILEDIR} is not a valid directory" && exit 1 )
+    FILECOUNT=$( find ./ -type f | wc -l)
+    STRCOUNT=$( grep -R -c "$SRCHSTR")
 
-    echo "Looking for file"
-
+    echo "The number of files are ${FILECOUNT}  and the number of matching lines are ${STRCOUNT} "
+    
 fi
